fbee0d78bcc26dd67c17d5eb99cd01ed6f9d372b
AlexisMoDz <40703952+AlexisMoDz@users.noreply.github.com>
2018-07-06 19:53:25 +0200

    Add files via upload

---
+++ boostvoiture/client/main.lua
@@ -0,0 +1,131 @@
+--[[----------BOOSTE VOITURE POLICE----------]]
+Citizen.CreateThread(function()
+   while true do
+    Citizen.Wait(0)
+    if IsVehicleModel(GetVehiclePedIsIn(GetPlayerPed(-1), true), GetHashKey("police")) then
+       SetVehicleEnginePowerMultiplier(GetVehiclePedIsIn(GetPlayerPed(-1), true), 2.9)
+       SetVehicleEngineTorqueMultiplier(GetVehiclePedIsIn(GetPlayerPed(-1), true), 2.9)
+     end
+   end
+ end)
+
+ Citizen.CreateThread(function()
+   while true do
+    Citizen.Wait(0)
+    if IsVehicleModel(GetVehiclePedIsIn(GetPlayerPed(-1), true), GetHashKey("police1")) then
+       SetVehicleEnginePowerMultiplier(GetVehiclePedIsIn(GetPlayerPed(-1), true), 2.9)
+       SetVehicleEngineTorqueMultiplier(GetVehiclePedIsIn(GetPlayerPed(-1), true), 2.9)
+     end
+   end
+ end)
+
+ Citizen.CreateThread(function()
+   while true do
+    Citizen.Wait(0)
+    if IsVehicleModel(GetVehiclePedIsIn(GetPlayerPed(-1), true), GetHashKey("police2")) then
+       SetVehicleEnginePowerMultiplier(GetVehiclePedIsIn(GetPlayerPed(-1), true), 2.9)
+       SetVehicleEngineTorqueMultiplier(GetVehiclePedIsIn(GetPlayerPed(-1), true), 2.9)
+     end
+   end
+ end)
+ 
+ Citizen.CreateThread(function()
+   while true do
+    Citizen.Wait(0)
+    if IsVehicleModel(GetVehiclePedIsIn(GetPlayerPed(-1), true), GetHashKey("police3")) then
+       SetVehicleEnginePowerMultiplier(GetVehiclePedIsIn(GetPlayerPed(-1), true), 2.9)
+       SetVehicleEngineTorqueMultiplier(GetVehiclePedIsIn(GetPlayerPed(-1), true), 2.9)
+     end
+   end
+ end)
+ 
+  Citizen.CreateThread(function()
+   while true do
+    Citizen.Wait(0)
+    if IsVehicleModel(GetVehiclePedIsIn(GetPlayerPed(-1), true), GetHashKey("police4")) then
+       SetVehicleEnginePowerMultiplier(GetVehiclePedIsIn(GetPlayerPed(-1), true), 2.9)
+       SetVehicleEngineTorqueMultiplier(GetVehiclePedIsIn(GetPlayerPed(-1), true), 2.9)
+     end
+   end
+ end)
+ 
+ Citizen.CreateThread(function()
+   while true do
+    Citizen.Wait(0)
+    if IsVehicleModel(GetVehiclePedIsIn(GetPlayerPed(-1), true), GetHashKey("fbi")) then
+       SetVehicleEnginePowerMultiplier(GetVehiclePedIsIn(GetPlayerPed(-1), true), 3.5)
+       SetVehicleEngineTorqueMultiplier(GetVehiclePedIsIn(GetPlayerPed(-1), true), 3.5)
+     end
+   end
+ end)
+ 
+ Citizen.CreateThread(function()
+   while true do
+    Citizen.Wait(0)
+    if IsVehicleModel(GetVehiclePedIsIn(GetPlayerPed(-1), true), GetHashKey("fbi2")) then
+       SetVehicleEnginePowerMultiplier(GetVehiclePedIsIn(GetPlayerPed(-1), true), 2.9)
+       SetVehicleEngineTorqueMultiplier(GetVehiclePedIsIn(GetPlayerPed(-1), true), 2.9)
+     end
+   end
+ end)
+ 
+  Citizen.CreateThread(function()
+   while true do
+    Citizen.Wait(0)
+    if IsVehicleModel(GetVehiclePedIsIn(GetPlayerPed(-1), true), GetHashKey("sheriff")) then
+       SetVehicleEnginePowerMultiplier(GetVehiclePedIsIn(GetPlayerPed(-1), true), 2.9)
+       SetVehicleEngineTorqueMultiplier(GetVehiclePedIsIn(GetPlayerPed(-1), true), 2.9)
+     end
+   end
+ end)
+ 
+  Citizen.CreateThread(function()
+   while true do
+    Citizen.Wait(0)
+    if IsVehicleModel(GetVehiclePedIsIn(GetPlayerPed(-1), true), GetHashKey("sheriff2")) then
+       SetVehicleEnginePowerMultiplier(GetVehiclePedIsIn(GetPlayerPed(-1), true), 2.9)
+       SetVehicleEngineTorqueMultiplier(GetVehiclePedIsIn(GetPlayerPed(-1), true), 2.9)
+     end
+   end
+ end) 
+ 
+  Citizen.CreateThread(function()
+   while true do
+    Citizen.Wait(0)
+    if IsVehicleModel(GetVehiclePedIsIn(GetPlayerPed(-1), true), GetHashKey("policeb")) then
+       SetVehicleEnginePowerMultiplier(GetVehiclePedIsIn(GetPlayerPed(-1), true), 2.9)
+       SetVehicleEngineTorqueMultiplier(GetVehiclePedIsIn(GetPlayerPed(-1), true), 2.9)
+     end
+   end
+ end) 
+ 
+  Citizen.CreateThread(function()
+   while true do
+    Citizen.Wait(0)
+    if IsVehicleModel(GetVehiclePedIsIn(GetPlayerPed(-1), true), GetHashKey("riot")) then
+       SetVehicleEnginePowerMultiplier(GetVehiclePedIsIn(GetPlayerPed(-1), true), 1.9)
+       SetVehicleEngineTorqueMultiplier(GetVehiclePedIsIn(GetPlayerPed(-1), true), 1.9)
+     end
+   end
+ end)
+ 
+   Citizen.CreateThread(function()
+   while true do
+    Citizen.Wait(0)
+    if IsVehicleModel(GetVehiclePedIsIn(GetPlayerPed(-1), true), GetHashKey("polchiron")) then
+       SetVehicleEnginePowerMultiplier(GetVehiclePedIsIn(GetPlayerPed(-1), true), 2.5)
+       SetVehicleEngineTorqueMultiplier(GetVehiclePedIsIn(GetPlayerPed(-1), true), 2.5)
+     end
+   end
+ end)
+--[[----------BOOSTE VOITURE NORMAL----------]]
+ 
+  Citizen.CreateThread(function()
+   while true do
+    Citizen.Wait(0)
+    if IsVehicleModel(GetVehiclePedIsIn(GetPlayerPed(-1), true), GetHashKey("Nom de la voiture")) then
+       SetVehicleEnginePowerMultiplier(GetVehiclePedIsIn(GetPlayerPed(-1), true), 2.5)
+       SetVehicleEngineTorqueMultiplier(GetVehiclePedIsIn(GetPlayerPed(-1), true), 2.5)
+     end
+   end
+ end)
